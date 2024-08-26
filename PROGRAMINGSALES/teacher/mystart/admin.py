b1ff2aaef21290fd0cdeed62568faa4db15d2b97
Guerly Macias <127213616+GuerlyMacias@users.noreply.github.com>
2024-08-26 17:21:04 -0500

    me

--- PROGRAMINGSALES/teacher/mystart/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User,Students_programs,TypeProgram
-
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Students_programs)
-admin.site.register(TypeProgram)
