78fc91d2193c9835acfd68525148898a0c5d24a1
Guerly Macias <127213616+GuerlyMacias@users.noreply.github.com>
2024-08-30 14:58:57 -0500

    background gradiente

--- preguntas/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User,biblia,pregunta
-# Register your models here.
-admin.site.register(User)
-admin.site.register(biblia)
-admin.site.register(pregunta)
